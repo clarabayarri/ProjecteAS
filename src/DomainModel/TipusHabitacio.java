33aa9fa9772b0a6226425632c9fad89933e56270
Lluís Gómez Hernando <lluisgh28@gmail.com>
2011-12-26 12:31:02 +0100

    Afegides les meves classes

---
+++ src/DomainModel/TipusHabitacio.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+/**
+ *
+ * @author lluisgh28
+ */
+public class TipusHabitacio {
+    
+}
