cbd2ce614effa4bd6b8193bbf5a2bbaa75539353
Lluís Gómez Hernando <lluisgh28@gmail.com>
2011-12-26 16:56:28 +0100

    Atributs CasUsReservarHabitacio

--- src/DomainModel/CasUsReservarHabitacio.java
+++ src/DomainModel/CasUsReservarHabitacio.java
@@ -4,10 +4,19 @@
  */
 package DomainModel;
 
+import java.util.Date;
+
 /**
  *
  * @author lluisgh28
  */
 public class CasUsReservarHabitacio {
-    
+    String poblacio;
+    Date dataInici;
+    Date dataFi;
+    String dniClient;
+    String nomHotel;
+    String tipusHabitacio;
+    Integer numHabitacio;
+    Float preuTotal;
 }
