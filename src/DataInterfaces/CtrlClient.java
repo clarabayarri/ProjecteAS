67f6033e28538c5f719d93931b56e0193eedb91d
Clara <nayami.ruka@gmail.com>
2011-12-27 11:11:19 +0100

    afegides les interficies de dades per habitacio i client, falten hotel i poblacio perque encara no existeixen les classes de domainmodel

--- src/DataInterfaces/CtrlClient.java
+++ src/DataInterfaces/CtrlClient.java
@@ -4,10 +4,18 @@
  */
 package DataInterfaces;
 
+import DomainModel.Client;
+
 /**
  *
  * @author clara
  */
 public interface CtrlClient {
     
+    /**
+     * Recupera un client a partir del seu dni
+     * @param dni
+     * @return 
+     */
+    public Client get(String dni);
 }
--- src/DataInterfaces/CtrlHabitacio.java
+++ src/DataInterfaces/CtrlHabitacio.java
@@ -4,10 +4,20 @@
  */
 package DataInterfaces;
 
+import DomainModel.Habitacio;
+
 /**
  *
  * @author clara
  */
 public interface CtrlHabitacio {
     
+    /**
+     * Recupera una habitacio a partir del nom de l'hotel i el numero de 
+     * l'habitacio
+     * @param numero
+     * @param nomHotel
+     * @return 
+     */
+    public Habitacio get(Integer numero, String nomHotel);
 }
