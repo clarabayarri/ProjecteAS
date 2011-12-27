16158dc1693515853ac304b307b417a6fc6ec28b
Clara <nayami.ruka@gmail.com>
2011-12-27 11:07:48 +0100

    acabada la classe bonshotels, falta saber que hem de fer amb getReservationId()

--- src/DomainModel/BonsHotels.java
+++ src/DomainModel/BonsHotels.java
@@ -10,4 +10,50 @@ package DomainModel;
  */
 public class BonsHotels {
     
+    private static BonsHotels singletonObject = null;
+    private float quotaFixa = 0;
+    private float quotaVariable = 0;
+    
+    /**
+     * Creadora privada, s'ha d'accedir des de fora a través de getInstance()
+     */
+    private BonsHotels() {
+        
+    }
+    
+    /**
+     * Recupera la instància singleton de la classe
+     * @return 
+     */
+    public static BonsHotels getInstance() {
+        if(singletonObject == null){
+            singletonObject = new BonsHotels();
+        }
+        return singletonObject;
+    }
+    
+    /**
+     * Assigna una quotaFixa
+     * @param quotaFixa 
+     */
+    public void setQuotaFixa(float quotaFixa) {
+        this.quotaFixa = quotaFixa;
+    }
+    
+    /**
+     * Assigna una quotaVariable
+     * @param quotaVariable 
+     */
+    public void setQuotaVariable(float quotaVariable) {
+        this.quotaVariable = quotaVariable;
+    }
+    
+    /**
+     * Recupera l'identificador de la reserva actual
+     * @return 
+     */
+    public String getReservationId() {
+        //TODO: arreglar aixo
+        return "";
+    }
 }
