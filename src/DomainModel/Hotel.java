d1ab91eef3f89ceaf8c0b7be73dfc5a98e66732c
elenaVazquez <evazquezdaza@gmail.com>
2011-12-28 17:40:26 +0100

    classes domini

---
+++ src/DomainModel/Hotel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+/**
+ *
+ * @author elena_gratallops
+ */
+public class Hotel {
+    
+}
