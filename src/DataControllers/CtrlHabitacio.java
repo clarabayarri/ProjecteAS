01c6bd9231538c91add67edccefeb876beeec1d4
Lluís Gómez Hernando <lluisgh28@gmail.com>
2012-01-07 16:45:47 +0100

    CtrlHabitacio

--- src/DataControllers/CtrlHabitacio.java
+++ src/DataControllers/CtrlHabitacio.java
@@ -2,17 +2,30 @@ package DataControllers;
 
 import DataInterfaces.ICtrlHabitacio;
 import DomainModel.Habitacio;
+import DomainModel.HabitacioId;
+import Hibernate.HibernateUtil;
+import org.hibernate.Session;
 
 /**
  * Contrlador de cada de gestió de dades encarregat de recuperar 
  * instancies d'habitacions.
  * @author clara
  */
-public class CtrlHabitacio implements ICtrlHabitacio{
+public class CtrlHabitacio implements ICtrlHabitacio {
 
     @Override
-    public Habitacio get(Integer numero, String nomHotel) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
+    public Habitacio get(Integer numero, String nomHotel) throws Exception {
+        Session session = null; 
+        Habitacio h = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            HabitacioId id = new HabitacioId(nomHotel, numero);
+            h = (Habitacio) session.get(Habitacio.class, id);
+        } catch (RuntimeException e) {
+            session.getTransaction().rollback();
+            throw new Exception("clientNoExisteix");
+        }
+        return h;
+    }    
 }
